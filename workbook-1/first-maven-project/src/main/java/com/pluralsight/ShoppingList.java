5b45fac1bfc48b8e54d5acd3913b5280d2781bf7
mruzomberka311 <gmruz311@gmail.com>
2024-09-25 13:25:57 -0400

    Initial Commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("milk, eggs, chicken, bacon, tea, salt, tomatoes, cereal, napkins, and paper towels");
+    }
+}
