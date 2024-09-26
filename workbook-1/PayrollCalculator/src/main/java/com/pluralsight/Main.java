7d8fe76b5e1d3831a6c749faf26e0933f3b868d7
mruzomberka311 <gmruz311@gmail.com>
2024-09-26 16:06:47 -0400

    Finished Payroll Calculator

---
+++ workbook-1/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter name");
+        String name = scanner.nextLine();
+
+
+
+        System.out.println("Please enter your hours you've worked");
+        float hours = scanner.nextFloat();
+
+        System.out.println("Please enter your pay rate");
+        float rate = scanner.nextFloat();
+
+        float pay = rate * hours;
+
+        System.out.print(pay);
+
+        scanner.nextLine();
+
+        System.out.print(" is your pay for the week " + name);
+
+
+
+
+
+    }
+}
