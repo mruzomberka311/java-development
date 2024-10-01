fd34e9fcba03a7b753dc9be0f75bf9293b20e5ad
mruzomberka311 <gmruz311@gmail.com>
2024-10-01 13:13:29 -0400

    corrections made for excersise

--- workbook-1/SandwichShop/src/main/java/com/pluralsight/Main.java
+++ workbook-1/SandwichShop/src/main/java/com/pluralsight/Main.java
@@ -7,30 +7,37 @@ public class Main {
 
         Scanner main = new Scanner(System.in);
         System.out.println("What kind of sandwich would you like?(1 or 2)");
-        double type = Scanner.nextInt();
+        int type = Scanner.nextInt();
 
+// mistake of not defining my basePrice variable
+        double basePrice;
         if (type == 1) {
-            return = 5.65;
-        } else if {(type == 2);
-            return  = 8.65;
-        } else{
+            basePrice = 5.65;
+        } else if (type == 2) {
+            basePrice = 8.65;
+        } else {
             System.out.println("Input Invalid");
+            return;
         }
 
-
+//mistake with format of if else statement
         System.out.println("Please enter your age");
         int age = Scanner.nextInt();
-
+//mistake of not correctly defining discountPrice variable
         double discountPercentage = 0;
 
-         if (age <= 17){
+        if (age <= 17) {
             discountPercentage = .10;
-         } else if (age >= 65)
-             discountPercentage = .20;
+        } else if (age >= 65)
+            discountPercentage = .20;
 
-          double finalPrice = type * (1- discountPercentage);
+
+        double finalPrice = type * (1 - discountPercentage);
 
         System.out.println(finalPrice);
+        main.close();
+
+
 
 
 
