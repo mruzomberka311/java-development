66f42ed5d0bdba7f89d00b16796eba8fb1f35348
mruzomberka311 <gmruz311@gmail.com>
2024-12-05 11:35:54 -0500

    Spring beanz

---
+++ workbook-9/NorthwindTradersSpringboot/src/main/java/com/pluralsight/NorthwindTradersSpringboot/NorthwindTradersSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTradersSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTradersSpringbootApplication.class, args);
+	}
+
+}
